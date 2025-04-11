ee6bb2e263e025571541ef2b9531d4d357278ecb
vajonam <152501+vajonam@users.noreply.github.com>
2025-04-11 08:56:23 -0400

    fixes for 2.3.0
    Signed-off-by: vajonam <152501+vajonam@users.noreply.github.com>

--- get_latest_orcalslicer_release.sh
+++ get_latest_orcalslicer_release.sh
@@ -2,12 +2,13 @@
 
 TMPDIR="$(mktemp -d)"
 
-curl -SsL https://api.github.com/repos/SoftFever/OrcaSlicer/releases/latest > $TMPDIR/latest.json
+# Fetch all releases (both latest stable and pre-releases)
+curl -SsL https://api.github.com/repos/SoftFever/OrcaSlicer/releases > $TMPDIR/releases.json
 
-# Grabs the first item of the latest result of the AppImage.
-url=$(jq -r '.assets[] | select(.browser_download_url|test("Linux_V.*AppImage$"))|.browser_download_url' $TMPDIR/latest.json)
-name=$(jq -r '.assets[] | select(.browser_download_url|test("Linux_V.*AppImage$"))|.name' $TMPDIR/latest.json)
-version=$(jq -r .tag_name $TMPDIR/latest.json)
+# Get the latest stable or pre-release that contains a matching AppImage
+url=$(jq -r '[.[] | select(.assets[].browser_download_url | test("Linux.*_Ubuntu2404_V.*AppImage$"))][0].assets[] | select(.browser_download_url | test("Linux.*_Ubuntu2404_V.*AppImage$")) | .browser_download_url' $TMPDIR/releases.json)
+name=$(jq -r '[.[] | select(.assets[].browser_download_url | test("Linux.*_Ubuntu2404_V.*AppImage$"))][0].assets[] | select(.browser_download_url | test("Linux.*_Ubuntu2404_V.*AppImage$")) | .name' $TMPDIR/releases.json)
+version=$(jq -r '[.[] | select(.assets[].browser_download_url | test("Linux.*_Ubuntu2404_V.*AppImage$"))][0].tag_name' $TMPDIR/releases.json)
 
 if [ $# -ne 1 ]; then
   echo "Wrong number of params"
@@ -35,4 +36,5 @@ case $request in
     ;;
 esac
 
+rm -rf $TMPDIR
 exit 0
